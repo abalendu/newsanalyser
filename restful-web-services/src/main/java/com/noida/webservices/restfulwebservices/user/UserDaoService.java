9893b310e982966526ecc3864c79982f2344d8fc
abalenduprakash <abalenduprakash@gmail.com>
2018-03-06 01:20:13 +0530

    Adding spring rest webservice

---
+++ restful-web-services/src/main/java/com/noida/webservices/restfulwebservices/user/UserDaoService.java
@@ -0,0 +1,43 @@
+package com.noida.webservices.restfulwebservices.user;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDaoService {
+
+	private static List<User> users = new ArrayList<>();
+	private static int userCounter = 2;
+	static {
+		users.add(new User(1, "namedl", new Date()));
+
+		users.add(new User(2, "named2", new Date()));
+	}
+
+	public List<User> findAll() {
+		return users;
+	}
+
+	public User save(User user) {
+		if (user.getId() == null) {
+			user.setId(++userCounter);
+
+		}
+		users.add(user);
+		return user;
+
+	}
+
+	public User findOne(int id) {
+		for (User user : users) {
+			if (user.getId() == id) {
+				return user;
+			}
+		}
+		return null;
+	}
+
+}
